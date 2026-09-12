c8fc5cab6412b224f26dafa4d3afc1edb4f5c58d
Daksh Patel <dakshp12345678@gmail.com>
2026-09-12 17:23:24 +0530

    Add request handler to integrate app with WSGI

--- api/index.py
+++ api/index.py
@@ -2,3 +2,6 @@ from app import create_app
 from app.config import ProductionConfig
 
 app = create_app(ProductionConfig)
+
+def handler(request, response):
+    return app(request.environ, response.start_response)
